07a33aa513d0fb1e4ded7a54fc99eee52f95f8ac
Alan Tan <atan@acerta.ai>
2024-02-05 00:46:01 -0500

    update 5.2.1.II.i.a

--- 5.2.1.II.i.a.py
+++ 5.2.1.II.i.a.py
@@ -1,5 +1,9 @@
 def func_demo_unpacking(arg1:str, arg2: str, arg3: str)->None:
     print(f"# received {arg1=}, {arg2=}, {arg3=}")
+    
 str_1 = "abc"
 print(f"# the string is: {str_1}")
+# the string is: abc
+
 func_demo_unpacking(*str_1)
+# received arg1='a', arg2='b', arg3='c'
