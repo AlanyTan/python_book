cb528a5eb5d11d9edbfbd1408383f895dc4a0645
Alan Tan <atan@acerta.ai>
2024-02-26 03:07:52 -0500

    update m_7_2_1

--- m7_2_1.py
+++ m7_2_1.py
@@ -1,6 +1,6 @@
 def main(file_name: str) -> None:
     file = open(file_name, 'w+', encoding='utf-8')
-    print("This is a test", "somemore info", 2, file=file)
+    print("Writing", "numbers like", 2, "booleans like", True, file=file)
     print(f"The filename of which I am writing into is {file_name}", file=file)
     file.close()
 
