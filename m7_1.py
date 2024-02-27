a4103d86a02228bc2d1e826ad6613788fff4040c
Alan Tan <atan@acerta.ai>
2024-02-26 22:15:14 -0500

    m7_1

--- m7_1.py
+++ m7_1.py
@@ -1,16 +1,18 @@
-def main() -> None:
-    file_w = open("example.tmp",'w', encoding='utf-8')
+def main(file_name: str) -> None:
+    file_w = open(file_name,'w', encoding='utf-8')
     print(f"# {type(file_w)=}, {file_w.closed=}")
     file_w.close()
     print(f"# {type(file_w)=}, {file_w.closed=}")
     
-    file_rb = open("example.tmp", 'r+b')
+    file_rb = open(file_name, 'r+b')
     print(f"# {type(file_rb)=}, {file_rb.closed=}")
     file_rb.close()
     print(f"# {type(file_rb)=}, {file_rb.closed=}")
 
 if __name__ == "__main__":
-    main()
+    base_name = __file__[:-3]
+    file_name = base_name + ".data.txt"
+    main(file_name)
 
 # type(file_w)=<class '_io.TextIOWrapper'>, file_w.closed=False
 # type(file_w)=<class '_io.TextIOWrapper'>, file_w.closed=True
--- m7_2_1.py
+++ m7_2_1.py
@@ -3,8 +3,8 @@ def main(file_name: str) -> None:
     bool_info = True
     text_info = "text like this"
     file_obj = open(file_name, 'w+', encoding='utf-8')
-    print("Saving", text_info, ", number", int_info, ", and boolean", bool_info,
-           file=file_obj)
+    print("Saving", text_info, ", number", int_info, ", and boolean", 
+          bool_info, file=file_obj)
     print(f"into file {file_name}",
            file=file_obj)
     file_obj.close()
