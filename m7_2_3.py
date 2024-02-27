0757e40fd29cb0e042e518e919a75c4cc0ac5c02
Alan Tan <atan@acerta.ai>
2024-02-26 22:59:28 -0500

    m7_2_3 add print write return

--- m7_2_3.py
+++ m7_2_3.py
@@ -4,20 +4,21 @@ def main(files: list[str]) -> None:
     text_info = "Python程序设计\n"
     file_name, open_mode, encoding_type = files[0]
     file_obj = open(file_name, "w+" + open_mode, encoding=encoding_type)
-    file_obj.write(f"Line 1, {text_info}")
-    file_obj.write(str(int_info))
-    file_obj.write("Line 3, string literal.\n")
-    file_obj.write(str(bool_info))
-    file_obj.write("The End.\n")
+    print(f"# .write() returned: {file_obj.write(f'Line 1, {text_info}')=}")
+    print(f"# .write() returned: {file_obj.write(str(int_info))=}")
+    print("# .write() returned:", file_obj.write("Line 3, string literal.\n"))
+    print(f"# .write() returned: {file_obj.write(str(bool_info))=}")
+    print("# .write() returned:", file_obj.write("The End.\n"))
     file_obj.close()
 
     file_name, open_mode, encoding_type = files[1]
     file_obj = open(file_name, "w+" + open_mode, encoding=encoding_type)
-    file_obj.write(bytes(f"Line 1, {text_info}",'utf-8'))
-    file_obj.write(int_info.to_bytes(4,'little'))
-    file_obj.write(bytearray("Line 3, string literal.\n", 'utf-8'))
-    file_obj.write(bytes([bool_info]))
-    file_obj.write(bytes("The End.\n", 'utf-8'))
+    print(f"# .write() returned: {file_obj.write(bytes(f'Line 1, {text_info}','utf-8'))=}")
+    print(f"# .write() returned: {file_obj.write(int_info.to_bytes(4,'little'))=}")
+    print("# .write() returned:", file_obj.write(
+        bytearray("Line 3, string literal.\n", 'utf-8')))
+    print(f"# .write() returned: {file_obj.write(bytes([bool_info]))=}")
+    print("# .write() returned:", file_obj.write(bytes("The End.\n", 'utf-8')))
     file_obj.close()
 
 if __name__ == "__main__":
@@ -25,3 +26,14 @@ if __name__ == "__main__":
     file_names = [(base_name + ".data.txt", "t", 'utf-8'),
                   (base_name + ".data.bin", "b", None)]
     main(file_names)
+
+# .write() returned: file_obj.write(f'Line 1, {text_info}')=19
+# .write() returned: file_obj.write(str(int_info))=1
+# .write() returned: 24
+# .write() returned: file_obj.write(str(bool_info))=4
+# .write() returned: 9
+# .write() returned: file_obj.write(bytes(f'Line 1, {text_info}','utf-8'))=27
+# .write() returned: file_obj.write(int_info.to_bytes(4,'little'))=4
+# .write() returned: 24
+# .write() returned: file_obj.write(bytes([bool_info]))=1
+# .write() returned: 9
