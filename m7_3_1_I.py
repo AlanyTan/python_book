1088b02035991650f1c22e276925f79b55086d2d
Alan Tan <atan@acerta.ai>
2024-02-27 03:02:13 -0500

    m_7_3_1_I

--- m7_3_1_I.py
+++ m7_3_1_I.py
@@ -1,9 +1,19 @@
 SKIP_WORD = 'skip'
 def main(file_name: str) -> None:
-    file = open(file_name, 'r', encoding='utf-8')
+    # first create the file with content
+    file_w = open(file_name, 'w+', encoding='utf-8')
+    file_content = [
+        "abc.dev\n",
+        "skip\n",
+        "123\n"
+    ]
+    file_w.writelines(file_content)
+    file_w.close()
+
+    #next read from the file in place of user input
+    file_r = open(file_name, 'r', encoding='utf-8')
     count = 0
-    content = ''
-    while content != 'exit':
+    while content := file_r.readline().strip("\n"):
         if content := '' if content == SKIP_WORD else content:
             print(f"# You entered: {content}")
             count += 1
@@ -12,34 +22,28 @@ def main(file_name: str) -> None:
             match letter:
                 case '.':
                     print(f"#..Reached period, abort rest.")
-                    content = ''
                     break
                 case '!':
                     print(f"#!!Reached exclaimation mark, aboar the whole thing!")          
-                    content = 'exit'
                     break
                 case _:
                     print(f"#   {letter}'s ASCII code is {ord(letter)}")
-        else:
-            content = file.readline().strip("\n") #input("#==Enter 'exit' to stop:")
         
     print(f"## You entered {count} strings in total")
-    file.close()
+    file_r.close()
 
 if __name__ == "__main__":
-    file_name = __file__ + ".data.txt"
+    base_name = __file__[:-3]
+    file_name = base_name + ".data.txt"
     main(file_name)
 
-#==Enter 'exit' to stop): abc.def
-# You entered: abc.def
+# You entered: abc.dev
 #   a's ASCII code is 97
 #   b's ASCII code is 98
 #   c's ASCII code is 99
 #..Reached period, abort rest.
-#==Enter 'exit' to stop): abc!def
-# You entered: abc!def
-#   a's ASCII code is 97
-#   b's ASCII code is 98
-#   c's ASCII code is 99
-#!!Reached exclaimation mark, aboar the whole thing!
+# You entered: 123
+#   1's ASCII code is 49
+#   2's ASCII code is 50
+#   3's ASCII code is 51
 ## You entered 2 strings in total
