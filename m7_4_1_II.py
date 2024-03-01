886bd90ae3bb4308ca6be6e7adfe5ef7b3cfe301
Alan Tan <atan@acerta.ai>
2024-03-01 00:42:08 -0500

    m7_4_1_II results

--- m7_4_1_II.py
+++ m7_4_1_II.py
@@ -21,8 +21,12 @@ def main(file_name: str) -> None:
     csv_reader = csv.DictReader(csv_file_obj, dialect='excel', 
                                 quoting=csv.QUOTE_NONNUMERIC)
     print(f"# open csv for {csv_file_obj.mode}, use {type(csv_reader)=}")
+    data_table = []
     for row in csv_reader:
         print("#", row)
+        data_table.append(row)
+    
+    print(f"# Locate Grade of #3 student: {data_table[3]['Grade']}")
         
     csv_file_obj.close()
 
@@ -37,3 +41,4 @@ if __name__ == '__main__':
 # {'ID': 2.0, 'Name': 'Bob', 'Age': 19.0, 'Grade': 70.0, 'Pass/Fail': 'True'}
 # {'ID': 3.0, 'Name': 'Carole', 'Age': 17.0, 'Grade': 75.0, 'Pass/Fail': 'True'}
 # {'ID': 4.0, 'Name': 'David', 'Age': 18.0, 'Grade': 85.5, 'Pass/Fail': 'True'}
+# Locate Grade of #3 student: 85.5,
