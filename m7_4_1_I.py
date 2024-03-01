9c438e1ca1c9e8825b612d55c939aead8d13e095
Alan Tan <atan@acerta.ai>
2024-02-29 22:40:52 -0500

    m_7_4_1_II corrected

--- m7_4_1_I.py
+++ m7_4_1_I.py
@@ -1,18 +1,20 @@
 import csv
 
 def main(file_name: str) -> None:
-    csv_file_obj = open(file_name, "a", newline='', encoding='utf-8')
+    csv_file_obj = open(file_name, "w", newline='', encoding='utf-8')
     csv_writer = csv.writer(csv_file_obj, dialect='excel')
+    print(f"# open csv for {csv_file_obj.mode}, use {type(csv_writer)=}")
     csv_writer.writerow(['ID', 'Name', 'Age', 'Grade', 'Pass/Fail'])
-    csv_writer.writerow([1, 'Alice', 18, 80.5, True])
-    csv_writer.writerow([2, 'Bob', 19, 70, True])
-    csv_writer.writerow([3, 'Carole', 17, 75, True])
-    csv_writer.writerow([4, 'David', 18, 85.5, True])
+    csv_writer.writerow([1, 'Alice', 18, 80.5, 'True'])
+    csv_writer.writerow([2, 'Bob', 19, 70, 'True'])
+    csv_writer.writerow([3, 'Carole', 17, 75, 'True'])
+    csv_writer.writerow([4, 'David', 18, 85.5, 'True'])
     csv_file_obj.close()
 
 
     csv_file_obj = open(file_name, "r", newline='', encoding='utf-8')
-    csv_reader = csv.reader(csv_file_obj, delimiter=',', quotechar='|')
+    csv_reader = csv.reader(csv_file_obj, dialect='excel')
+    print(f"# open csv for {csv_file_obj.mode}, use {type(csv_reader)=}")
     for row in csv_reader:
         print("#", row)
         
@@ -23,8 +25,11 @@ if __name__ == '__main__':
     file_name = (base_name + ".data.csv")
     main(file_name)
 
+# open csv for w, use type(csv_writer)=<class '_csv.writer'>
+# open csv for r, use type(csv_reader)=<class '_csv.reader'>
 # ['ID', 'Name', 'Age', 'Grade', 'Pass/Fail']
 # ['1', 'Alice', '18', '80.5', 'True']
 # ['2', 'Bob', '19', '70', 'True']
 # ['3', 'Carole', '17', '75', 'True']
 # ['4', 'David', '18', '85.5', 'True']
+    
--- m7_4_1_II.py
+++ m7_4_1_II.py
@@ -2,14 +2,22 @@ import csv
 
 def main(file_name: str) -> None:
     csv_file_obj = open(file_name, "w", newline='', encoding='utf-8')
-    csv_writer = csv.writer(csv_file_obj, dialect='excel')
+    fieldnames = ['ID', 'Name', 'Age', 'Grade', 'Pass/Fail']
+    csv_writer = csv.DictWriter(csv_file_obj, fieldnames, dialect='excel', quoting=csv.QUOTE_NONNUMERIC)
     print(f"# open csv for {csv_file_obj.mode}, use {type(csv_writer)=}")
+    csv_writer.writeheader()
+    csv_writer.writerow({'ID':1, 'Name':'Alice', 'Age':18, 'Grade':80.5, 'Pass/Fail':str(True)})
+    csv_writer.writerow({'ID':2, 'Name':'Bob', 'Age':19, 'Grade':70, 'Pass/Fail':str(True)})
+    csv_writer.writerow({'ID':3, 'Name':'Carole', 'Age':17, 'Grade':75, 'Pass/Fail':str(True)})
+    csv_writer.writerow({'ID':4, 'Name':'David', 'Age':18, 'Grade':85.5, 'Pass/Fail':str(True)})
     csv_file_obj.close()
 
 
     csv_file_obj = open(file_name, "r", newline='', encoding='utf-8')
-    csv_reader = csv.reader(csv_file_obj, delimiter=',', quotechar='|')
+    csv_reader = csv.DictReader(csv_file_obj, dialect='excel', quoting=csv.QUOTE_NONNUMERIC)
     print(f"# open csv for {csv_file_obj.mode}, use {type(csv_reader)=}")
+    for row in csv_reader:
+        print("#", row)
         
     csv_file_obj.close()
 
@@ -18,5 +26,9 @@ if __name__ == '__main__':
     file_name = (base_name + ".data.csv")
     main(file_name)
 
-# open csv for w, use type(csv_writer)=<class '_csv.writer'>
-# open csv for r, use type(csv_reader)=<class '_csv.reader'>
+# open csv for w, use type(csv_writer)=<class 'csv.DictWriter'>
+# open csv for r, use type(csv_reader)=<class 'csv.DictReader'>
+# {'ID': 1.0, 'Name': 'Alice', 'Age': 18.0, 'Grade': 80.5, 'Pass/Fail': 'True'}
+# {'ID': 2.0, 'Name': 'Bob', 'Age': 19.0, 'Grade': 70.0, 'Pass/Fail': 'True'}
+# {'ID': 3.0, 'Name': 'Carole', 'Age': 17.0, 'Grade': 75.0, 'Pass/Fail': 'True'}
+# {'ID': 4.0, 'Name': 'David', 'Age': 18.0, 'Grade': 85.5, 'Pass/Fail': 'True'}
