b19550f5681d53ec57703a3b89a91fd1615f6916
Alan Tan <atan@acerta.ai>
2024-03-02 02:40:08 -0500

    m7_4_2 booltype

--- m7_4_2.py
+++ m7_4_2.py
@@ -4,18 +4,18 @@ import json
 def main(file_name: str) -> None:
     json_file_obj = open(file_name, "w", newline='', encoding='utf-8')
     student_record_table = [('ID', 'Name', 'Age', 'Grade', 'Pass/Fail'),
-                    (1, 'Alice', 18.0, 80.5, 'True'),
-                    (2, 'Bob', 19.0, 70.0, 'True'),
-                    (3, 'Carole', 17.0, 75.0, 'True'),
-                    (4, 'David', 18.0, 85.5, 'True')]
+                    (1, 'Alice', 18.0, 80.5, True),
+                    (2, 'Bob', 19.0, 70.0, True),
+                    (3, 'Carole', 17.0, 75.0, True),
+                    (4, 'David', 18.0, 85.5, True)]
     student_record_dict = [{'ID': 1, 'Name': 'Alice', 'Age': 18.0, 
-                            'Grade': 80.5, 'Pass/Fail': 'True'},
+                            'Grade': 80.5, 'Pass/Fail': True},
                     {'ID': 2, 'Name': 'Bob', 'Age': 19.0, 
-                     'Grade': 70.0, 'Pass/Fail': 'True'},
+                     'Grade': 70.0, 'Pass/Fail': True},
                     {'ID': 3, 'Name': 'Carole', 'Age': 17.0, 
-                     'Grade': 75.0, 'Pass/Fail': 'True'},
+                     'Grade': 75.0, 'Pass/Fail': True},
                     {'ID': 4, 'Name': 'David', 'Age': 18.0, 
-                     'Grade': 85.5, 'Pass/Fail': 'True'}]
+                     'Grade': 85.5, 'Pass/Fail': True}]
     json.dump((student_record_table, student_record_dict), json_file_obj)
     
     json_file_obj.close()
@@ -34,11 +34,11 @@ if __name__ == '__main__':
     main(file_name)
 
 # type(stu_rec)=<class 'list'>: ['ID', 'Name', 'Age', 'Grade', 'Pass/Fail']
-# type(stu_rec)=<class 'list'>: [1, 'Alice', 18.0, 80.5, 'True']
-# type(stu_rec)=<class 'list'>: [2, 'Bob', 19.0, 70.0, 'True']
-# type(stu_rec)=<class 'list'>: [3, 'Carole', 17.0, 75.0, 'True']
-# type(stu_rec)=<class 'list'>: [4, 'David', 18.0, 85.5, 'True']
-# type(stu_rec)=<class 'dict'>: {'ID': 1, 'Name': 'Alice', 'Age': 18.0, 'Grade': 80.5, 'Pass/Fail': 'True'}
-# type(stu_rec)=<class 'dict'>: {'ID': 2, 'Name': 'Bob', 'Age': 19.0, 'Grade': 70.0, 'Pass/Fail': 'True'}
-# type(stu_rec)=<class 'dict'>: {'ID': 3, 'Name': 'Carole', 'Age': 17.0, 'Grade': 75.0, 'Pass/Fail': 'True'}
-# type(stu_rec)=<class 'dict'>: {'ID': 4, 'Name': 'David', 'Age': 18.0, 'Grade': 85.5, 'Pass/Fail': 'True'}
+# type(stu_rec)=<class 'list'>: [1, 'Alice', 18.0, 80.5, True]
+# type(stu_rec)=<class 'list'>: [2, 'Bob', 19.0, 70.0, True]
+# type(stu_rec)=<class 'list'>: [3, 'Carole', 17.0, 75.0, True]
+# type(stu_rec)=<class 'list'>: [4, 'David', 18.0, 85.5, True]
+# type(stu_rec)=<class 'dict'>: {'ID': 1, 'Name': 'Alice', 'Age': 18.0, 'Grade': 80.5, 'Pass/Fail': True}
+# type(stu_rec)=<class 'dict'>: {'ID': 2, 'Name': 'Bob', 'Age': 19.0, 'Grade': 70.0, 'Pass/Fail': True}
+# type(stu_rec)=<class 'dict'>: {'ID': 3, 'Name': 'Carole', 'Age': 17.0, 'Grade': 75.0, 'Pass/Fail': True}
+# type(stu_rec)=<class 'dict'>: {'ID': 4, 'Name': 'David', 'Age': 18.0, 'Grade': 85.5, 'Pass/Fail': True}
