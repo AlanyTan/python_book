e0ef0f9bc3906f4c4dc6ffeefbf916a0dd9f3501
Alan Tan <atan@acerta.ai>
2024-04-02 02:45:42 -0400

    4.4.4.II

--- 4_4_4_II.py
+++ 4_4_4_II.py
@@ -1,4 +1,15 @@
 def greet(desc: str, name: str = "World", seq: int = None) -> None:
+    """The function prints a greeting message based on the provided parameters.
+
+    Args:
+        description: required description of how the function was called.
+        name: the name of the person to greet, if omitted, will use value 'World'.
+        seq: the sequence number of the time greeted, if omitted, will print
+            'lost tract' as the start of the greeting
+    
+    Returns:
+        None.
+    """
     if seq is None:
         print(f"# lost track: Hello, {name}! ({desc})")
     else:
