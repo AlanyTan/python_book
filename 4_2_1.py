7c7dcb9b6605488dabc1692674e6ea9e0bad9d9a
Alan Tan <atan@acerta.ai>
2024-04-01 22:51:09 -0400

    4.2.1 space

--- 4_2_1.py
+++ 4_2_1.py
@@ -1,10 +1,10 @@
 def circle_area(x) -> float:
     PI = 3.14159
-    circumference = 2 * PI * x 
+    circumference = 2*PI*x
     return circumference
 
 def rectangle_area(length, width) -> int|float:
     return length * width
 
-def print_rectangle_area(length,width) -> None:
-    print(f"area of the rectangle is:{length * width}")
+def print_rectangle_area(length, width) -> None:
+    print(f"area of the rectangle is:{length*width}")
