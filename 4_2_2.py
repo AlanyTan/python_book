5d71dcabc6297c2fca4f6a93fa90f20a4c95d8f0
Alan Tan <atan@acerta.ai>
2024-04-01 21:47:17 -0400

    4.2.2 update docstring

--- 4_2_2.py
+++ 4_2_2.py
@@ -1,9 +1,13 @@
 def f(x)->float:
     """
-    Calculate the area of a circle with a radius of x.
+    Calculate the circumference of a circle with a radius of x.
 
     Args:
-      x:  the radius of the circle, an int or a float
+      x:  int | float
+        the radius of the circle
+
+    Returns:
+      A float representing the circumference of the circle.
     """
     PI = 3.14159
     circumference = 2 * PI * x 
