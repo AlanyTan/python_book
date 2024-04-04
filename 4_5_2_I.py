bdc5cb209f67cac48d1006c69564df530bca0def
Alan Tan <atan@acerta.ai>
2024-04-04 01:31:46 -0400

    4.5.2 enclsing scope

--- 4_5_2_I.py
+++ 4_5_2_I.py
@@ -1,33 +1,40 @@
 def func_parent_scope(arg_p: str) -> str:
+    """Demonstrate enclosing scope reading
+
+    Args:
+        arg_p: argument to parent function
+
+    Returns:
+        the string returned by func_child which should be the 
+            value of local_var
+    """
     enclosing_var = 'encv'
     def func_child_scope(arg_c: int) -> str:
         local_var = 'locv'
-        print(f"#   -= func_child, arg_c exist: {'arg_c' in dir()\
-        }, \t{arg_c=}")
-        print(f"#   -= func_child, arg_p exist: {'arg_p' in dir()\
-        }, \t{arg_p=}")
-        print(f"#   -= func_child, local_var exist: {'local_var' in dir()\
-        }, \t{local_var=}")
-        print(f"#   -= func_child, enclosing_var exist: {'enclosing_var' \
-        in dir()}, \t{enclosing_var=}")
+        print(f"#  -=func_child, arg_c exist: {'arg_c' in dir()}, {arg_c=}")
+        print(f"#  -=func_child, arg_p exist: {'arg_p' in dir()}, {arg_p=}")
+        print(f"#  -=func_child, local_var exist: {'local_var' in dir()}"
+              f", {local_var=}")
+        print(f"#  -=func_child, enclosing_var exist:"
+              f" {'enclosing_var' in dir()}, {enclosing_var=}")
         return local_var
     
-    print(f"# - func_parent, arg_p exist: {'arg_p' in dir()}, \t{arg_p=}")
-    print(f"# - func_parent, enclosing_var exist: {'enclosing_var' in dir()\
-        }, \t{enclosing_var=}")
-    print(f"# - func_parent, arg_c exist: {'arg_c' in dir()}")
-    print(f"# - func_parent, local_var exist: {'local_var' in dir()},")
-    return func_child_scope(0)
+    print(f"# -func_parent, arg_p exist: {'arg_p' in dir()}, {arg_p=}")
+    print(f"# -func_parent, enclosing_var exist: {'enclosing_var' in dir()},"
+          f" {enclosing_var=}")
+    print(f"# -func_parent, arg_c exist: {'arg_c' in dir()}")
+    print(f"# -func_parent, local_var exist: {'local_var' in dir()},")
+    return func_child_scope('from parent')
 
-print(f"# func returned:", func_parent_scope("anything"))
-# - func_parent, arg_p exist: True, 	arg_p='anything'
-# - func_parent, enclosing_var exist: True, 	enclosing_var='encv'
-# - func_parent, arg_c exist: False
-# - func_parent, local_var exist: False,
-#   -= func_child, arg_c exist: True, 	arg_c=0
-#   -= func_child, arg_p exist: True, 	arg_p='anything'
-#   -= func_child, local_var exist: True, 	local_var='locv'
-#   -= func_child, enclosing_var exist: True, 	enclosing_var='encv'
+print(f"# func returned:", func_parent_scope("from main"))
+# -func_parent, arg_p exist: True, arg_p='from main'
+# -func_parent, enclosing_var exist: True, enclosing_var='encv'
+# -func_parent, arg_c exist: False
+# -func_parent, local_var exist: False,
+#  -=func_child, arg_c exist: True, arg_c='from parent'
+#  -=func_child, arg_p exist: True, arg_p='from main'
+#  -=func_child, local_var exist: True, local_var='locv'
+#  -=func_child, enclosing_var exist: True, enclosing_var='encv'
 # func returned: locv
 
 print(f"# outside, arg_p exist: {'arg_p' in dir()}")
--- 4_5_2_II.py
+++ 4_5_2_II.py
@@ -1,18 +1,41 @@
 def func_parent_scope(arg_p: str) -> str:
+    """Demonstrate enclosing scope assigning
+
+    Args:
+        arg_p: argument to parent function
+
+    Returns:
+        the string returned by func_child which should be the 
+            value of local_var
+    """
+    shadow_var = 'shdv'
+    shd_var_alias = shadow_var
     enclosing_var = 'encv'
     enc_var_alias = enclosing_var
     def func_child_scope(arg_c: int) -> int:
         nonlocal enclosing_var
-        enclosing_var = "value from child"
-        local_var = 'locv'
+        print(f"#  -=func_child, {enclosing_var is enc_var_alias=}"
+              f", {enclosing_var=}")
+        enclosing_var = "enclosing_var from child"
+        print(f"#  -=func_child, {enclosing_var is enc_var_alias=}"
+              f", {enclosing_var=}")
+        shadow_var = 'shadow_var from child'
+        print(f"#  -=func_child, {shadow_var is shd_var_alias=}")
         return arg_c
 
     print(f"# -func_parent, {enclosing_var is enc_var_alias=}, {enclosing_var=}")
+    print(f"# -func_parent, {shadow_var is shd_var_alias=}, {shadow_var=}")
     func_child_scope(0)
     print(f"# -func_parent, {enclosing_var is enc_var_alias=}, {enclosing_var=}")
+    print(f"# -func_parent, {shadow_var is shd_var_alias=}, {shadow_var=}")
     return arg_p[:3]
 
 print(f"# func returned:", func_parent_scope("anything"))
 # -func_parent, enclosing_var is enc_var_alias=True, enclosing_var='encv'
-# -func_parent, enclosing_var is enc_var_alias=False, enclosing_var='value from child'
+# -func_parent, shadow_var is shd_var_alias=True, shadow_var='shdv'
+#  -=func_child, enclosing_var is enc_var_alias=True, enclosing_var='encv'
+#  -=func_child, enclosing_var is enc_var_alias=False, enclosing_var='enclosing_var from child'
+#  -=func_child, shadow_var is shd_var_alias=False
+# -func_parent, enclosing_var is enc_var_alias=False, enclosing_var='enclosing_var from child'
+# -func_parent, shadow_var is shd_var_alias=True, shadow_var='shdv'
 # func returned: any
