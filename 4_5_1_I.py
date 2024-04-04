d427b880c4cd4351a789aa38c31a2e22ce8e4f39
Alan Tan <atan@acerta.ai>
2024-04-04 00:22:46 -0400

    4.5.1 local scope

--- 4_5_1_I.py
+++ 4_5_1_I.py
@@ -1,17 +1,25 @@
 def func_local_scope(arg_l: str) -> str:
+    """Demonstrate variable's local scope
+    
+    Args:
+        arg_l: an argument used to demonstrate arguments are local scoped
+        
+    Returns:
+        A string that was the value of the local variable.
+    """
+
     local_var = 'locv'
-    print(f"# - func_local, arg_l exist: {'arg_l' in dir()}, {arg_l=}")
-    print(f"# - func_local, local_var exist: {'local_var' in dir()},\
-        {local_var=}")
+    print(f"# -in func_local_scope, arg_l exist: {'arg_l' in dir()}, {arg_l=}")
+    print(f"# -in func_local_scope, local_var exist: {'local_var' in dir()}"
+          f", {local_var=}")
     return local_var
 
-print(f"# func returned:", func_local_scope("anything"))
-# - func_local, arg_l exist: True, arg_l='anything'
-# - func_local, local_var exist: True,        local_var='locv'
-# func returned: locv
 
-print(f"# outside, arg_l exist: {'arg_l' in dir()}")
-# outside, arg_l exist: False
+print(f"# in main, {func_local_scope('anything')=}")
+# -in func_local_scope, arg_l exist: True, arg_l='anything'
+# -in func_local_scope, local_var exist: True, local_var='locv'
+# in main, func_local_scope('anything')='locv'
 
-print(f"# outside func, local_var exist: {'local_var' in dir()}")
-# outside func, local_var exist: False    
+print(f"# in main, arg_l exist: {'arg_l' in dir()}"
+      f", local_var exist: {'local_var' in dir()}")
+# in main, arg_l exist: False, local_var exist: False
--- 4_5_1_II.py
+++ 4_5_1_II.py
@@ -1,14 +1,23 @@
 def func_local_scope(arg_l: str) -> str:
-    var = 0
-    print(f"# - func_local, var exist: {var is var_alias=}, {var=}")    
-    return arg_l[:3]
+    """Demonstrate variable shadowing
+    
+    Args:
+        arg_l: an argument used to demonstrate arguments are local scoped
+        
+    Returns:
+        A string that was the value of the arg_l 
+    """
+
+    var = "local scope"
+    print(f"# -in func_local_scope, {var is var_alias=}, {var=}, {var_alias=}")
+    return arg_l
+
 
 var = "main scope"
 var_alias = var
+print(f"# in main, var exist: {'var' in dir()}, {var is var_alias=}, {var=}")
+# in main, var exist: True, var is var_alias=True, var='main scope'
 
-print(f"# func returned:", func_local_scope("anything"))
-# - func_local, var exist: var is var_alias=False, var=0
-# func returned: any
-
-print(f"# outside, var exist: {'var' in dir()}, {var is var_alias=}, {var=}")
-# outside, var exist: True, var is var_alias=True, var='main scope'
+print(f"# in main, {func_local_scope(var)=}")
+# -in func_local, var is var_alias=False, var='local scope', var_alias='main scope'
+# in main, func_local_scope(var)='main scope'
