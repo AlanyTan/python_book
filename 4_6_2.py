047fe0649a2af57d0e756a89a0c59aa3ddb1fb45
Alan Tan <atan@acerta.ai>
2024-04-04 22:33:21 -0400

    4.6.2 docstring

--- 4_6_2.py
+++ 4_6_2.py
@@ -2,7 +2,11 @@ def func_side_effect(arg_1: bytearray, arg_2: bytearray) -> str:
     """Demonstrate side effect of passing arguments.
 
     Args:
-        arg_1: 
+        arg_1: any bytearray.
+        arg_2: a bytearray to be changed by func_child.
+
+    Returns:
+        string representation of mutated arg_2.
     """
     arg_1.extend(b'changed by func')
     local_var = arg_1.copy()
@@ -17,7 +21,7 @@ print(f"# before func call {var_1=}, {var_2=}" )
 # before func call var_1=bytearray(b''), var_2=bytearray(b'value 1, set by main')
 
 print(f"# {func_side_effect(var_1, var_2)=}")
-# func_side_effect(var_1, var_2)='value changed by func'
+# func_side_effect(var_1, var_2)='value 2, changed by func'
 
 print(f"# after func call {var_1=}, {var_2=}" )
-# after func call var_1=bytearray(b'changed by func'), var_2=bytearray(b'value2, changed by func')
+# after func call var_1=bytearray(b'changed by func'), var_2=bytearray(b'value 2, changed by func')
