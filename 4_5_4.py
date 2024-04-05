1b29ed2305d6bc9f4f5b558425b16c8746b400f8
Alan Tan <atan@acerta.ai>
2024-04-04 22:05:49 -0400

    4.6 side-effect

--- 4_5_4.py
+++ 4_5_4.py
@@ -16,10 +16,10 @@ def func_shadow_buildins() -> None:
 
 
 print(f"# in main before, {len('abc')=}, {__file__=}")
-# in main before, len('abc')=3, __file__='/home/alan/Documents/Python.book/4_5_4.py'
+# in main before, len('abc')=3, __file__='/home/user/Documents/Python.book/4_5_4.py'
 
 func_shadow_buildins()
 # -=in func_shadow_buildins: len('abc')='len() has been shadowed.', __file__='shadowed filename.'
 
 print(f"# in main after, {len('abc')=}, {__file__=}")
-# in main after, len('abc')=3, __file__='/home/alan/Documents/Python.book/4_5_4.py'
+# in main after, len('abc')=3, __file__='/home/user/Documents/Python.book/4_5_4.py'
