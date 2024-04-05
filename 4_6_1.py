38849809cf90d9c62147f8f1f72e4ea8615d5373
Alan Tan <atan@acerta.ai>
2024-04-04 22:06:20 -0400

    4.6 side-effect

--- 4_6_1.py
+++ 4_6_1.py
@@ -1,15 +1,22 @@
 def func_side_effect(arg_p: str) -> str:
+    """Demonstrate side effect.
+
+    Args:
+        arg_s: incoming string to be processed.
+
+    Returns:
+        arg_s repeated global_var_in times.
+    """
     global global_var_out
-    multiplier = int(len(global_var_in)) % 5
+    multiplier = global_var_in + 1
     global_var_out = multiplier
-    return arg_p[:3] * multiplier
+    return arg_p * multiplier
 
-global_var_in = 'value 1 from main'
+global_var_in = 1
 global_var_out = 'old value'
-print(f"# {func_side_effect("any")=}, {global_var_out=}" )
-# func_side_effect("any")='anyany', global_var_out=2
+print(f"# {func_side_effect('any')=}, {global_var_out=}" )
+# func_side_effect('any')='anyany', global_var_out=2
 
-global_var_in = 'value' * global_var_out 
-global_var_out = 'old value'
-print(f"# {func_side_effect("any")=}, {global_var_out=}" )
-# func_side_effect("any")='', global_var_out=0
+global_var_in = 2
+print(f"# {func_side_effect('any')=}, {global_var_out=}" )
+# func_side_effect('any')='anyanyany', global_var_out=3
--- 4_6_2.py
+++ 4_6_2.py
@@ -1,14 +1,23 @@
 def func_side_effect(arg_1: bytearray, arg_2: bytearray) -> str:
-    arg_1.extend(b'child')
-    arg_1 = ord('9')
+    """Demonstrate side effect of passing arguments.
+
+    Args:
+        arg_1: 
+    """
+    arg_1.extend(b'changed by func')
+    local_var = arg_1.copy()
+    arg_1 = ord('2')
     arg_2[6]= arg_1
-    arg_2 += b"'s child"
+    arg_2[8:] = local_var
     return arg_2.decode()
 
-var_1 = bytearray(1)
-var_2 = bytearray(b'value 1 from main')
+var_1 = bytearray(0)
+var_2 = bytearray(b'value 1, set by main')
+print(f"# before func call {var_1=}, {var_2=}" )
+# before func call var_1=bytearray(b''), var_2=bytearray(b'value 1, set by main')
+
 print(f"# {func_side_effect(var_1, var_2)=}")
-# func_side_effect(var_1, var_2)="value 9 from main's child"
+# func_side_effect(var_1, var_2)='value changed by func'
 
 print(f"# after func call {var_1=}, {var_2=}" )
-# after func call var_1=bytearray(b'\x00child'), var_2=bytearray(b"value 9 from main\'s child")
+# after func call var_1=bytearray(b'changed by func'), var_2=bytearray(b'value2, changed by func')
