9994093866230c7c716036dfc969289efe6f63bf
Alan Tan <alan@alantan.com>
2024-04-06 05:07:56 -0400

    6.3 docstring

---
+++ m6_3_package/__init__.py
@@ -0,0 +1,19 @@
+"""Learning Python Package.
+
+This package provides basic concepts of how Python package
+and its modules are organized.
+
+Modules:
+    circle: module include functions to calculate area and
+            circumference of a circle
+    rectangle: module include functions to calculate area
+            and perimeter of a rectangle.
+
+Usage:
+    >>> from m6_3_package import circle
+    >>> circle_area = circle.area(4)
+    >>> from m6_3_package import rectangle
+    >>> rect_perimeter = rectangle.perimeter(2,3)
+
+For more details on each module, refer to the module docstring.
+"""
