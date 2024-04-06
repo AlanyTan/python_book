e7b698ca00b521cbd29593099e99b087d9bdaa84
Alan Tan <atan@acerta.ai>
2024-04-06 03:55:49 -0400

    6.2.4 import in func

--- m6_2_4.py
+++ m6_2_4.py
@@ -1,4 +1,9 @@
-#save this as m6_2_2_IV.py
+"""Main script demonstrating importing into current namespace.
+
+No functions, just demo from module import obj will bring obj into 
+current namespace.
+"""
+"""save this as m6_2_4.py"""
 from m6_1_circle import area as circle_area, circumference
 
 print(f"# {'PI' in dir()=}, {'radius' in dir()=}")
@@ -15,7 +20,10 @@ print(f"# {'area' in dir()=}, {'circle_area' in dir()=},"
       f" {circle_area(radius)=}")
 # 'area' in dir()=False,'circle_area' in dir()=True, circle_area(radius)=28.27433385
 
+def import_in_func() -> None:
+      """Demo import can be done inside a function"""
+      from m6_1_circle import PI
+      print(f"# {'PI' in locals()=},{'PI' in globals()=}, {PI=}")
 
-from m6_1_circle import PI
-print(f"# {'PI' in dir()=}, {PI=}")
-# 'PI' in dir()=True, PI=3.14159265
+import_in_func()
+# 'PI' in locals()=True,'PI' in globals()=False, PI=3.14159265
