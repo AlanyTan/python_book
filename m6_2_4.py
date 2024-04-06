8978efd211e79313534689de0b98c417ff143e60
Alan Tan <atan@acerta.ai>
2024-04-06 04:19:55 -0400

    6.2.4 wrap line

--- m6_2_4.py
+++ m6_2_4.py
@@ -12,14 +12,15 @@ print(f"# {'global_var' in dir()=}")
 print(f"# {'check_global_var' in dir()=}, {check_global_var()=}")
 # 'check_global_var' in dir()=True, check_global_var()='value set in m6_2_scope'
 
-print(f"# {'len' in __builtins__=}, {'len' in dir()=}, {'m6_2_scope_len' in dir()=},"
-      f" {m6_2_scope_len(1.2)=}")
+print(f"# {'len' in __builtins__=}, {'len' in dir()=}"
+      f", {'m6_2_scope_len' in dir()=}, {m6_2_scope_len(1.2)=}")
 # 'len' in __builtins__=True, 'len' in dir()=False, 'm6_2_scope_len' in dir()=True, m6_2_scope_len(1.2)='LEN1.2'
 
 def import_in_func() -> None:
       """Demo import can be done inside a function"""
       from m6_2_scope import global_var
-      print(f"# {'global_var' in locals()=},{'global_var' in globals()=}, {global_var=}")
+      print(f"# {'global_var' in locals()=},{'global_var' in globals()=}"
+            f", {global_var=}")
 
 import_in_func()
 # 'global_var' in locals()=True,'global_var' in globals()=False, global_var='value set in m6_2_scope'
