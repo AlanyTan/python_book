a3464220d6764649aa07d815b1599ddd9e38ccb0
Alan Tan <atan@acerta.ai>
2024-04-06 04:01:52 -0400

    6.2.4 switch to use 6.2.scope

--- m6_2_4.py
+++ m6_2_4.py
@@ -4,26 +4,22 @@ No functions, just demo from module import obj will bring obj into
 current namespace.
 """
 """save this as m6_2_4.py"""
-from m6_1_circle import area as circle_area, circumference
+from m6_2_scope import len as m6_2_scope_len, check_global_var
 
-print(f"# {'PI' in dir()=}, {'radius' in dir()=}")
-# 'PI' in dir()=False, 'radius' in dir()=False
+print(f"# {'global_var' in dir()=}")
+# 'global_var' in dir()=False
 
-radius = 3
-print(f"# {'radius' in dir()=}, {radius=}")
-# 'radius' in dir()=True, radius=3
+print(f"# {'check_global_var' in dir()=}, {check_global_var()=}")
+# 'check_global_var' in dir()=True, check_global_var()='value set in m6_2_scope'
 
-print(f"# {'circumference' in dir()=}, {circumference(radius)=}")
-# 'circumference' in dir()=True, circumference(radius)=18.849555900000002
-
-print(f"# {'area' in dir()=}, {'circle_area' in dir()=},"
-      f" {circle_area(radius)=}")
-# 'area' in dir()=False,'circle_area' in dir()=True, circle_area(radius)=28.27433385
+print(f"# {'len' in dir()=}, {'m6_2_scope_len' in dir()=},"
+      f" {m6_2_scope_len(1.2)=}")
+# 'len' in dir()=False, 'm6_2_scope_len' in dir()=True, m6_2_scope_len(1.2)='LEN1.2'
 
 def import_in_func() -> None:
       """Demo import can be done inside a function"""
-      from m6_1_circle import PI
-      print(f"# {'PI' in locals()=},{'PI' in globals()=}, {PI=}")
+      from m6_2_scope import global_var
+      print(f"# {'global_var' in locals()=},{'global_var' in globals()=}, {global_var=}")
 
 import_in_func()
-# 'PI' in locals()=True,'PI' in globals()=False, PI=3.14159265
+# 'global_var' in locals()=True,'global_var' in globals()=False, global_var='value set in m6_2_scope'
