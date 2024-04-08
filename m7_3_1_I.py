89d014774d415b46a368c4a67bfc157e72f8aff2
Alan Tan <atan@acerta.ai>
2024-04-07 20:02:00 -0400

    7.3.1 readline and readlines

--- m7_3_1_I.py
+++ m7_3_1_I.py
@@ -1,21 +1,55 @@
+"""Module demonstrate using .readline() replacing input().
+
+This module keeps the main program logic as section 3.4 but replaces
+input() with .readline() so it takes input from a text file.
+
+Usage: 
+    python -m m7_3_1_I
+
+Note:
+    This module will create a test file under the current directory, 
+    so please make sure you have write permission in the current dir.
+"""
 SKIP_WORD = 'skip'
-def main(file_name: str) -> None:
-    # first create the file with content
-    file_w = open(file_name, 'w+', encoding='utf-8')
-    file_content = [
-        "abc.dev\n",
+EXAMPLE_CONTENT = ["abc.dev\n",
         "\n",
         "skip\n",
-        "123\n"
+        "123\n",
     ]
+def creat_input_file(file_name: str, file_content: list = None) -> bool:
+    """Create file with given file_name and write prepared content to it.
+
+    Args: 
+        file_name: the name of the file to create and write into.
+
+    Returns:
+        True, if the creation and writing were successful.    
+    """
+    if file_content is None:
+        file_content = EXAMPLE_CONTENT
+    file_w = open(file_name, 'w+', encoding='utf-8')
     file_w.writelines(file_content)
     file_w.close()
+    return True
 
-    #next read from the file in place of user input
+def main(file_name: str) -> None:
+    """Demo using .readline() read from file to replace input()
+    
+    This function demonstrate replacing input() with .readline() but 
+    keep all other aspects of the program logic intact.
+    
+    Args:
+        file_name: the name of the text file to use.
+        
+    Returns:
+        None
+    """
     file_r = open(file_name, 'r', encoding='utf-8')
     count = 0
     break_again = False
     while content := file_r.readline():
+    # the above while can be replaced by a for loop with exact same effect
+    # for content in file_r:
         content = content.strip("\n")
         if content := '' if content == SKIP_WORD else content:
             print(f"# You entered: {content}")
@@ -42,7 +76,8 @@ def main(file_name: str) -> None:
 if __name__ == "__main__":
     base_name = __file__[:-3]
     file_name = base_name + ".data.txt"
-    main(file_name)
+    if creat_input_file(file_name):
+        main(file_name)
 
 # You entered: abc.dev
 #   a's ASCII code is 97
--- m7_3_1_II.py
+++ m7_3_1_II.py
@@ -1,5 +1,33 @@
+"""Module demonstrate using .readlines() and list operations to process file content.
+
+This module realize the same functionality as m7_3_1_I, but replace sequential line
+processing with .readlines() and list, and generators.
+
+Usage: 
+    python -m m7_3_1_II
+
+Note:
+    This module will create a test file under the current directory, 
+    so please make sure you have write permission in the current dir.
+"""
+from m7_3_1_I import creat_input_file
+
 SKIP_WORD = ['skip']
+
 def parse_line(line:str ) -> list:
+    """Parse a line based on logic in section 3.4
+
+    For each char in the given line, add its ASCII code to return list.
+    Unless it's a '.' or '!' which will abort and ignore everything after it.
+    If the line is '\n' or only contain words in the SKIP_WORD list, then 
+    the result list will be empty.
+
+    Args:
+        line: the str that contains characters to be processed.
+
+    Returns:
+        A list of str, each shows the char and its ASCII code.
+    """
     result = []
     line = line.strip('\n')
     if line := '' if line in SKIP_WORD else line:
@@ -12,52 +40,39 @@ def parse_line(line:str ) -> list:
                 break
             case '!':
                 result.append(f"#!!Reached exclaimation mark, aboar the whole thing!")          
-                break_again = True
                 break
             case _:
                 result.append(f"#   {letter}'s ASCII code is {ord(letter)}")
     return result
 
 def main(file_name: str) -> None:
-    # first create the file with content
-    file_w = open(file_name, 'w+', encoding='utf-8')
-    file_content = [
-        "abc.dev\n",
-        "\n",
-        "skip\n",
-        "123\n"
-    ]
-    file_w.writelines(file_content)
-    file_w.close()
-
-    #next read from the file in place of user input
+    """Demo using .readlines() read full content from file.
+    
+    This function demonstrate using .readlines() to read file lines into
+    a list, and use list, map, generator to process the content, but 
+    realize the program behavior.
+    
+    Args:
+        file_name: the name of the text file to use.
+        
+    Returns:
+        None
+    """
     file_r = open(file_name, 'r', encoding='utf-8')
-    count = 0
-    break_again = False
     contents = file_r.readlines()
-    result = map(parse_line, contents)
+    exclaimation_marks = [i for i, s in enumerate(contents) if '!' in s]
+    if exclaimation_marks:
+        contents[exclaimation_marks[0]+1:]=[]
+    result = map(lambda line: map(print, parse_line(line)), contents)
 
-    for item in result:
-        #print(item)
-        if item:
-            count += 1
-
-        for it in item:
-            print(it)
-            if it == f"#!!Reached exclaimation mark, aboar the whole thing!":
-                break_again = True
-                break
-
-        if break_again:
-            break
-
-    print(f"## You entered {count} strings in total")
+    print(f"## You entered {sum(1 for item in result if list(item))} strings in total")
     file_r.close()
 
 if __name__ == "__main__":
     base_name = __file__[:-3]
     file_name = base_name + ".data.txt"
-    main(file_name)
+    if creat_input_file((file_name)):
+        main(file_name)
 
 # You entered: abc.dev
 #   a's ASCII code is 97
