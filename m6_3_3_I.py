e8d34358bba5288125f63de5ff0fb0731a2f69da
Alan Tan <atan@acerta.ai>
2024-07-09 17:24:10 +0000

    6.3.3.I

--- m6_3_3_I.py
+++ m6_3_3_I.py
@@ -3,26 +3,40 @@
 import m6_3_package
 #  in m6_3_package __init__.py
 
-print("#=root_level_local_namespace globals:"
+print("#=1.root level globals:"
         , *[(k, v) for k, v in globals().items() if not k.startswith('__')]
         , sep='\n# ')
-#=root_level_local_namespace globals:
+#=1.root level globals:
 # ('m6_3_package', <module 'm6_3_package' from '/workspaces/python-book/m6_3_package/__init__.py'>)
+print("#=1.root level dir(m6_3_package)", [d for d in dir(m6_3_package) if not d.startswith('__')])
+#=1.root level dir(m6_3_package) ['func_in_init']
+print("#=1.root level call func_in_init:", m6_3_package.func_in_init())
+#=1.root level call func_in_init: in func_in_init of m6_3_package
 
-print("#=root level dir(m6_3_package)", [d for d in dir(m6_3_package) if not d.startswith('__')])
-#=root level dir(m6_3_package) ['m6_3_package']
-print("#=root level", m6_3_package.func_in_init())
-#=root level in func_in_init of m6_3_package
+import m6_3_package.module_1 as mod_1
+#  in m_package module_1.py ['var_mod_1']
+print("#=2.root level globals:"
+        , *[(k, v) for k, v in globals().items() if not k.startswith('__')]
+        , sep='\n# ')
+#=2.root level globals:
+# ('m6_3_package', <module 'm6_3_package' from '/workspaces/python-book/m6_3_package/__init__.py'>)
+# ('mod_1', <module 'm6_3_package.module_1' from '/workspaces/python-book/m6_3_package/module_1.py'>)
+print("#=2.root level dir(m6_3_package)", [d for d in dir(m6_3_package) if not d.startswith('__')])
+#=root level dir(m6_3_package) ['func_in_init', 'module_1']
+print("#=2.root level dir(mod_1)", [d for d in dir(mod_1) if not d.startswith('__')])
+#=root level dir(mod_1) ['func_in_mod_1', 'var_mod_1']
+print(f"#=2.root level {mod_1.func_in_mod_1 is m6_3_package.module_1.func_in_mod_1=}")
+#=root level mod_1.func_in_mod_1 is m6_3_package.module_1.func_in_mod_1=True
 
 import m6_3_package.m6_3_2_geometry.circle
 #   in m6_3_2_package/m6_3_2_geometry/__init__.py ['CONST_1', 'circle', 'rectangle']
 import m6_3_package.m6_3_2_geometry.rectangle as rect
-print("#=root level dir(m6_3_package)", [d for d in dir(m6_3_package) if not d.startswith('__')])
+print("#=3.root level dir(m6_3_package)", [d for d in dir(m6_3_package) if not d.startswith('__')])
 #=root level dir(m6_3_package) ['func_in_init', 'm6_3_2_geometry']
-print("#==package level dir(.m6_3_2_geometry)", 
+print("#==3.package level dir(.m6_3_2_geometry)", 
       [d for d in dir(m6_3_package.m6_3_2_geometry) if not d.startswith('__')])
 #==package level dir(.m6_3_2_geometry) ['CONST_1', 'circle', 'rectangle']
-print("#===subpackage level dir(..circle)", 
+print("#===3.subpackage level dir(..circle)", 
       [d for d in dir(m6_3_package.m6_3_2_geometry.circle) if not d.startswith('__')])
 #===subpackage level dir(..circle) ['PI', 'area', 'circumference', 'main']
 
