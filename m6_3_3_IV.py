6c00903b154751d2dc3d72f28e578f609862a781
Alan Tan <atan@acerta.ai>
2024-07-10 04:33:59 +0000

    6..3.3.IV

--- m6_3_3_IV.py
+++ m6_3_3_IV.py
@@ -4,7 +4,12 @@ from m6_3_package.m6_3_2_geometry.circle import area as circle_area
 #  in m6_3_package __init__.py
 #   in m6_3_2_package/m6_3_2_geometry/__init__.py ['CONST_1', 'circle', 'rectangle']
 from m6_3_package.m6_3_2_geometry.rectangle import perimeter
-c#=root level dir() ['circle_area', 'perimeter']
+print("#=1.root_level_local_namespace globals:"
+        , *[(k, v) for k, v in globals().items() if not k.startswith('__')]
+        , sep='\n# ')
+#=1.root_level_local_namespace globals:
+# ('circle_area', <function area at 0x7a841f229f80>)
+# ('perimeter', <function perimeter at 0x7a841f22b600>)
 
 radius = 3
 print(f"# area of {radius=} is {circle_area(radius)}")
