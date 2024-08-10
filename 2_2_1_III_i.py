839c2773843b2dea7fa4c0385be43aa402c49a9c
Alan Tan <atan@acerta.ai>
2024-08-10 17:04:22 +0000

    2.2 & 2.3 pep8

--- 2_2_1_III_i.py
+++ 2_2_1_III_i.py
@@ -1,7 +1,7 @@
 x = 10
 print("#", x, "is of type", type(x))
 # 10 is of type <class 'int'>
- 
+
 x = x / 2
 print("#", x, "is of type", type(x))
 # 5.0 is of type <class 'float'>
--- 2_3_1_II.py
+++ 2_3_1_II.py
@@ -1,7 +1,7 @@
 x = 10
 y = 3.0
 z = (2 + 1j)
- 
+
 sum_ = x + x
 print("#", sum_, type(sum_))
 # 20 <class 'int'>
@@ -13,7 +13,7 @@ print("#", difference, type(difference))
 product = x * z
 print("#", product, type(product))
 # (20+10j) <class 'complex'>
-      
+
 quotient = x / 2
 print("#", quotient, type(quotient))
 # 5.0 <class 'float'>
@@ -21,11 +21,11 @@ print("#", quotient, type(quotient))
 exponential = z ** 2
 print("#", exponential, type(exponential))
 # (3+4j) <class 'complex'>
-      
+
 euclidean_quotient = x // y
 print("#", euclidean_quotient, type(euclidean_quotient))
 # 3.0 <class 'float'>
-      
+
 remainder = x % 4
 print("#", remainder, type(remainder))
 # 2 <class 'int'>
