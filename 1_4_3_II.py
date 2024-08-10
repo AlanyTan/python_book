1de8159c8f83dbd263995638101c394334dc813e
Alan Tan <atan@acerta.ai>
2024-08-10 06:57:58 +0000

    update chapter 1 for pep8

--- 1_4_3_II.py
+++ 1_4_3_II.py
@@ -1,7 +1,8 @@
 def func_indent(additional_text):
     if True:
-        text= "Indentation is vital in Python!"
+        text = "Indentation is vital in Python!"
         print(text, additional_text)
     return "function executed"
+
 
 print(func_indent("That's right!"))
--- 1_4_3_III.py
+++ 1_4_3_III.py
@@ -1,10 +1,10 @@
 total = 1 +\
-        2 +\
-        3
-print(
-    total
-    )
-multi_line_text="""\
+    2 +\
+    3
+print("#",
+      total
+      )
+multi_line_text = """\
 String in multi-line-text
 can span across multiple lines,
 it can have quotation marks like "
