78a2101511926fc16bff826afd8a0c38b0ee4395
Alan Tan <atan@acerta.ai>
2024-08-19 20:49:01 +0000

    5_5_2 add set.difference

--- 5_5_2_III.py
+++ 5_5_2_III.py
@@ -1,17 +1,22 @@
-set_1 = {1, "2", 3.0}
-set_2 = {"2", 1}
-set_3 = {1, "4", 3.0}
-set_4 = {1, "2", 1, "2"}
+set_1_alias = set_1 = {1, "2", 3.0}
+set_2 = {3, "4", 1}
+set_2_copy = {*set_2}
+print(f"# {set_1 - set_2=}, {set_1 ^ set_2=}")
+# set_1 - set_2={'2'}, set_1 ^ set_2={'4', '2'}
 
-print(f"# {set_1 > set_2=}, {set_1 >= set_2=}, {set_1 <= set_2=}")
-# set_1 > set_2=True
-print(f"# {set_4 == set_2=}, {set_2 >= set_4=}, {set_2 <= set_4=}")
-# set_4 == set_2=True, set_2 >= set_4=True, set_2 <= set_4=True
-print(f"# {set_1 == set_3=}")
-# set_1 == set_3=False
-print(f"# {set_1 < set_3=}")
-# set_1 < set_3=False
-print(f"# {set_1 > set_3=}")
-# set_1 > set_3=False
-print(f"# {(set_1 != set_3)=}")
-# (set_1 != set_3)=True
+set_2 -= set_1
+print(f"# {set_2=}, {set_2_copy=}")
+# set_2={'4'}, set_2_copy={1, '4', 3}
+set_1 -= set_2_copy
+print(f"# {set_1=}, {set_1 is set_1_alias=}")
+# set_1={'2'}, set_1 is set_1_alias=True
+
+set_1_alias = set_1 = {1, "2", 3.0}
+set_2 = {3, "4", 1}
+set_2_copy = {*set_2}
+set_2 ^= set_1
+print(f"# {set_2=}, {set_2_copy=}")
+# set_2={'4', '2'}, set_2_copy={1, 3, '4'}
+set_1 ^= set_2_copy
+print(f"# {set_1=}, {set_1 is set_1_alias=}")
+# set_1={'4', '2'}, set_1 is set_1_alias=True
--- 5_5_2_IV.py
+++ 5_5_2_IV.py
@@ -1,3 +1,17 @@
-odd_squares = {x * x for x in range(-5, 6) if x % 2}
-print(f"# {odd_squares=}")
-# odd_squares={25, 9, 1}
+set_1 = {1, "2", 3.0}
+set_2 = {"2", 1}
+set_3 = {1, "4", 3.0}
+set_4 = {1, "2", 1, "2"}
+
+print(f"# {set_1 > set_2=}, {set_1 >= set_2=}, {set_1 <= set_2=}")
+# set_1 > set_2=True
+print(f"# {set_4 == set_2=}, {set_2 >= set_4=}, {set_2 <= set_4=}")
+# set_4 == set_2=True, set_2 >= set_4=True, set_2 <= set_4=True
+print(f"# {set_1 == set_3=}")
+# set_1 == set_3=False
+print(f"# {set_1 < set_3=}")
+# set_1 < set_3=False
+print(f"# {set_1 > set_3=}")
+# set_1 > set_3=False
+print(f"# {(set_1 != set_3)=}")
+# (set_1 != set_3)=True
