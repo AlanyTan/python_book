9b64078ea8f74d48ebca981f5c9ae37e04013201
Alan Tan <atan@acerta.ai>
2024-08-19 22:54:47 +0000

    5.5.1.III add frozenset examples

--- 5_5_1_III.py
+++ 5_5_1_III.py
@@ -1,8 +1,15 @@
 string_example = "AbCdEfABCDEF"
 tuple_example = (1, 2, "3", "four", 5.0, 1)
 
-print(f"# convert string to list: {set(string_example)}")
-# convert string to set: {'F', 'C', 'A', 'D', 'f', 'E', 'd', 'b', 'B'}
+print(f"# convert string to: {set(string_example)}")
+# convert string to: {'d', 'E', 'B', 'D', 'b', 'f', 'F', 'A', 'C'}
 
-print(f"# convert range to list: {set(tuple_example)}")
-# convert range to set: {1, 2, 5.0, '3', 'four'}
+print(f"# convert tuple to: {set(tuple_example)}")
+# convert tuple to: {1, 2, 5.0, 'four', '3'}
+
+print(f"# convert range to: {frozenset(range(4))}")
+# convert range to: frozenset({0, 1, 2, 3})
+
+print(f"# convert filtered to: {frozenset(filter(lambda x: x <= 'Z',
+                                                 string_example))}")
+# convert set to: frozenset({'E', 'B', 'D', 'F', 'A', 'C'})
